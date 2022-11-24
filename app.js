443d3103005850bfe608408391b8c6001d220807
scienceqiu <scienceqiu@gmail.com>
2022-11-24 17:08:06 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const port = 8080;
+
+const axios = require('axios');
+
+
+app.use(express.urlencoded({ extended: true}));
+
+app.get("/run", async (req, res) => {
+    console.log("IT WORKED");
+
+    //make this post and run code here, return verdict. still do queueing in the main vm
+
+
+    res.send("WOW THIS IS A RESPONSE");
+});
+
+async function makeReq() {
+await axios.get('http://10.202.0.2/')
+    .then(res => {
+        console.log(res);
+    }).catch((error) => {
+        console.log(error);
+    });
+}
+
+//makeReq();
+
+
+console.log("start");
+app.listen(port);
