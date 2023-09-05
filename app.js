20d2cabd2c3f60df500610dcfef7692a4a3b5ffc
Daniel Qiu <scienceqiu@gmail.com>
2023-09-05 21:02:04 +0000

    edited

--- app.js
+++ app.js
@@ -1,32 +1,52 @@
 require('dotenv').config();
 const express = require("express");
-const codRouter= require("./routes/handle");
 const app = express();
 const port = 8080;
+const {compileTests, run} = require("./routes/runCode");
 
 const axios = require('axios');
 
 
 app.use(express.urlencoded({ extended: true}));
 
-app.get("/run", async (req, res) => {
-    console.log("IT WORKED");
 
-    //make this post and run code here, return verdict. still do queueing in the main vm
-
-
-    res.send("WOW THIS IS A RESPONSE");
+app.get("/wow", async (req, res) => {
+	console.log("hi!");
+	res.send("WOW");
 });
 
-app.use("/", codRouter);
-async function makeReq() {
-await axios.get('http://10.202.0.2/')
-    .then(res => {
-        console.log(res);
-    }).catch((error) => {
-        console.log(error);
-    });
-}
+app.post("/lol", async (req, res) => {
+	console.log(req);
+	res.send("daniel orz";)
+});
+
+app.post("/run", async (req, res) => {
+	console.log("IT WORKED");
+	console.log(req.body);
+	//let language = req.body.lang;
+	console.log(language);
+	if (language != 'python' && language != 'cpp' && language != 'java') {
+		console.log("bad");
+		res.send("unacceptable code language");
+		return;
+	}
+
+	let pid = req.body.problemid;
+	if(pid ==""){
+		res.send("WHAT PROBLEM STUPID");
+		return;
+	}
+	let file = req.body.code;
+	code = {"code": req.body.code, "language":language}
+	problem = {"id": 1, "tl" : 1000, "ml" : 1000, "checkid": 1}
+
+	result= await run(problem, code);
+	console.log("YA");
+	console.log(result);
+	res.send(result)
+	//make this post and run code here, return verdict. still do queueing in the main vm
+});
+
 
 //makeReq();
 
--- routes/runCode.js
+++ routes/runCode.js
@@ -18,34 +18,32 @@ async function compileTests(problem, tests){
         let lang = problem.lang;
 	let lst = [];
 	for(let i = 0; i<tests.length; i++){
-		fs.writeFileSync('test.in', tests[i].test);
+		fs.writeFileSync('test.in', tests[i].test); //maybe get rid of this?
+		fs.writeFileSync(loc+"/test/"+i, tests[i].test);
 		console.log("tests", tests[i].test);
+		let output = ''
 		if (lang== 'cpp') {
-			fs.writeFileSync('routes/subcode/test.cpp', solution);
+			fs.writeFileSync('subcode/test.cpp', solution);
 			//write to correct file for code
 			//output = execSync('test.in<sudo ./nsjail/nsjail --config nsjail/configs/executable.cfg', { encoding: 'utf-8' });  //pipe input into this
 		}
 		else if (lang== 'python') {
 			console.log(solution);
 			console.log("running python");
-			fs.writeFileSync('routes/subcode/hello.py', solution);
+			fs.writeFileSync('subcode/hello.py', solution);
 			try {
-				//output = await execSync('sudo ./nsjail/nsjail --config nsjail/configs/python.cfg > test.in', { encoding: 'utf-8' });
-				//JOHNNY I CHNGED UR CODE CUZ IT WOULDNT COMPILE SRY
 				output = execSync('sudo ./nsjail/nsjail --config nsjail/configs/python.cfg < test.in', { encoding: 'utf-8' });
 			}
 			catch (error) {
 				console.log("ERROR", error);
 			}
 			console.log("output was", output);
-			fs.writeFileSync(loc+"/sol/"+i, output);
 		}
 		else if (lang== 'java') {
-			fs.writeFileSync('routes/subcode/test.java', solution);
+			fs.writeFileSync('subcode/test.java', solution);
 			output = execSync('sudo ./nsjail/nsjail --config nsjail/configs/java.cfg', { encoding: 'utf-8' });  
 		}
-		//fs.writeFileSync('test.cpp', solCode);
-		//addSol(i.id, ans);
+		fs.writeFileSync(loc+"/sol/"+i, output);
 	}
 }
 
@@ -54,78 +52,35 @@ function sleep(ms) {
     setTimeout(resolve, ms);
   });
 }
-async function run() {
-        if (tasks.length == 0) {
-                running = false;
-                return;
-        }
-        let task = tasks.shift();
-        let sub = tasksS.shift();
-        console.log(task, sub);
-        let res = await grabProblem(task);
-        let tests = await grabTests(task);
-        let checkid = res.checkid;
-        let tl = res.tl;
-        let ml = res.ml;
-        res = await grabStatus(sub);
-        let userCode = res.code;
-
-        let language = res.language;
-
-        let output = undefined, fverdict = "AC", runtime = 420, memory = 100;
-
-        console.log("\n\n\n-------------------");
-
-
-        await axios.get('http://10.202.0.3/run')
-        .then(res => {
-                console.log(res);
-        }).catch((error) => {
-                console.log(error);
-        });
-
-
-        console.log("THIS IS FAKE RUNNING. FIRST \n\n\n\n");
-        await sleep(10000);
-        console.log("SECOND \n\n\n\n");
-
-
-        insertSubmission(sub, fverdict, runtime, memory);
-        run();
+function grabTests(task){
+	return [1, 2]
 }
+function grabChecker(cid){
+}
+async function run(problem, submit) {
+	let loc = "../problems/"+problem.id;
+        let tests = await grabTests(task)
+        let checkid = problem.checkid;
+        let tl = problem.tl;
+        let ml = problem.ml;
+        let userCode = submit.code;
 
-async function realRun() {
-        if (tasks.length == 0) {
-                running = false;
-                return;
-        }
-        let task = tasks.shift();
-        let sub = tasksS.shift();
-        console.log(task, sub);
-        let res = await grabProblem(task);
-        let tests = await grabTests(task);
-        let checkid = res.checkid;
-        let tl = res.tl;
-        let ml = res.ml;
-        res = await grabStatus(sub);
-        let userCode = res.code;
+        let language = submit.language;
 
-        let language = res.language;
-
-        let output = undefined, fverdict = "AC", runtime = 420, memory = 100;
+        let output = undefined, fverdict = "AC"
 
         for(let i = 0; i<tests.length; i++){
                 let verdict = undefined;
                 fs.writeFileSync('test.in', tests[i].test);
                 if (language == 'cpp') {
-                        fs.writeFileSync('routes/subcode/test.cpp', userCode);
+                        fs.writeFileSync('subcode/test.cpp', userCode);
                         //write to correct file for code
-                        let wow = execSync('g++ routes/subcode/test.cpp');
+                        let wow = execSync('g++ subcode/test.cpp');
                         output = execSync('sudo ./nsjail/nsjail --config nsjail/configs/executable.cfg < test.in', { encoding: 'utf-8' });  //pipe input into this
                 }
                 else if (language == 'python') {
                         console.log("running python");
-                        fs.writeFileSync('routes/subcode/hello.py', userCode);
+                        fs.writeFileSync('subcode/hello.py', userCode);
                         try {
                                 //output = await execSync('sudo ./nsjail/nsjail --config nsjail/configs/python.cfg > test.in', { encoding: 'utf-8' });
                                 //JOHNNY I CHNGED UR CODE CUZ IT WOULDNT COMPILE SRY
@@ -141,21 +96,22 @@ async function realRun() {
                         output = execSync('sudo ./nsjail/nsjail --config nsjail/configs/java.cfg', { encoding: 'utf-8' });  
                 }
                 console.log("output was", output);
+		/**
                 fs.writeFileSync('test.in', output);
                 fs.writeFileSync('test.out', tests[i].ans);
-                let check= await grabChecker(checkid);
+                let check= grabChecker(checkid);
                 let ccode = check.code;
                 let lang= check.lang;
                 if (lang== 'cpp') {
-                        fs.writeFileSync('routes/subcode/test.cpp', ccode);
-                        let wow = execSync('g++ routes/subcode/test.cpp');
+                        fs.writeFileSync('subcode/test.cpp', ccode);
+                        let wow = execSync('g++ subcode/test.cpp');
                         //write to correct file for code
                         output = execSync('sudo ./nsjail/nsjail --config nsjail/configs/executable.cfg', { encoding: 'utf-8' });  //pipe input into this
                 }
                 else if (lang== 'python') {
                         console.log(solution);
                         console.log("running python");
-                        fs.writeFileSync('routes/subcode/hello.py', ccode);
+                        fs.writeFileSync('subcode/hello.py', ccode);
                         output = execSync('sudo ./nsjail/nsjail --config nsjail/configs/python.cfg < test.in', { encoding: 'utf-8' });
                         //updateTestSol(tests[i].id, output);
                 }
@@ -169,17 +125,17 @@ async function realRun() {
                         fverdict = "WA";
                         break;
                 }
+		**/
         }
-        insertSubmission(sub, fverdict, runtime, memory);
 
         //checker = undefined;
         userCode = undefined;
         //input = undefined;
-        run();
+	return fverdict
 }
 
 module.exports = {
-        queue: (pid, sid) => {
+        run: (pid, sid) => {
                 return queue(pid, sid);
         },
         compileTests: (problem, tests) => {
