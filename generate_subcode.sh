d13a021f76d837951305e875ee999c77bf721762
Samuel Chow <samuel.chow.45@gmail.com>
2025-06-29 17:56:25 -0400

    edit gensubcode

--- generate_subcode.sh
+++ generate_subcode.sh
@@ -1,4 +1,6 @@
-mkdir -p subcode
-touch subcode/test.cpp
-touch subcode/test.py
-touch subcode/test.java
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+mkdir -p $SCRIPT_DIR/subcode
+touch $SCRIPT_DIR/subcode/test.cpp
+touch $SCRIPT_DIR/subcode/test.py
+touch $SCRIPT_DIR/subcode/test.java
+
